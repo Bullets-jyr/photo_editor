ebecff1e2a2c489e341b4f74e75a95ac02e074fb
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-01-31 20:12:50 +0900

    feat: Select Image

---
+++ lib/screens/start_screen.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:photo_editor/helper/app_image_picker.dart';
+import 'package:photo_editor/provider/app_image_provider.dart';
+import 'package:provider/provider.dart';
+
+class StartScreen extends StatefulWidget {
+  const StartScreen({super.key});
+
+  @override
+  State<StartScreen> createState() => _StartScreenState();
+}
+
+class _StartScreenState extends State<StartScreen> {
+  late AppImageProvider imageProvider;
+
+  @override
+  void initState() {
+    imageProvider = Provider.of<AppImageProvider>(context, listen: false);
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            width: double.infinity,
+            child: Image.asset(
+              'assets/images/wallpaper.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            children: [
+              Expanded(
+                child: Center(
+                  child: Text(
+                    'Photo Editor',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 5,
+                      wordSpacing: 10,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(),
+              ),
+              Expanded(
+                child: Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {
+                          AppImagePicker(source: ImageSource.gallery).pick(
+                            onPick: (File? image) {
+                              // print(image!.path);
+                              imageProvider.changeImage(image!);
+                              Navigator.of(context).pushReplacementNamed('/home');
+                            },
+                          );
+                        },
+                        child: Text(
+                          'Gallery',
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          AppImagePicker(source: ImageSource.camera).pick(
+                            onPick: (File? image) {
+                              // print(image!.path);
+                              imageProvider.changeImage(image!);
+                              Navigator.of(context).pushReplacementNamed('/home');
+                            },
+                          );
+                        },
+                        child: Text(
+                          'Camera',
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
