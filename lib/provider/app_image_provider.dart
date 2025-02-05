dd2283ded4bdf3770448ae006903433dee70104f
Bullets-jyr <jy.ryu.jiyoung@gmail.com>
2025-02-05 18:05:50 +0900

    feat: Save Image, Undo, Redo

--- lib/provider/app_image_provider.dart
+++ lib/provider/app_image_provider.dart
@@ -4,15 +4,63 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 
 class AppImageProvider extends ChangeNotifier {
-  Uint8List? currentImage;
+  // Uint8List? _currentImage;
+
+  List<Uint8List> _images = [];
+  int _index = 0;
+
+  bool canUndo = false;
+  bool canRedo = false;
 
   changeImageFile(File image) {
-    currentImage = image.readAsBytesSync();
-    notifyListeners();
+    // _currentImage = image.readAsBytesSync();
+    _add(image.readAsBytesSync());
+    // notifyListeners();
   }
 
   changeImage(Uint8List image) {
-    currentImage = image;
+    // _currentImage = image;
+    _images.add(image);
+    _add(image);
+    // notifyListeners();
+  }
+
+  Uint8List? get currentImage {
+    // return _currentImage;
+    return _images[_index];
+  }
+
+  _add(Uint8List image) {
+    if (_images.isEmpty) {
+      _images.add(image);
+    } else {
+      int removeUntil = (_images.length - 1) - _index;
+      _images.length = _images.length - removeUntil;
+      _images.add(image);
+      _index++;
+    }
+    _undoRedo();
     notifyListeners();
+  }
+
+  undo() {
+    if (_index > 0) {
+      _index--;
+    }
+    _undoRedo();
+    notifyListeners();
+  }
+
+  redo() {
+    if (_index < (_images.length - 1)) {
+      _index++;
+    }
+    _undoRedo();
+    notifyListeners();
+  }
+
+  _undoRedo() {
+    canUndo = (_index != 0) ? true : false;
+    canRedo = (_index < (_images.length - 1)) ? true : false;
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+// import 'package:image_gallery_saver/image_gallery_saver.dart';
+import 'package:image_gallery_saver_plus/image_gallery_saver_plus.dart';
 import 'package:photo_editor/provider/app_image_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -10,6 +13,36 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late AppImageProvider imageProvider;
+
+  @override
+  void initState() {
+    imageProvider = Provider.of<AppImageProvider>(context, listen: false);
+    super.initState();
+  }
+
+  _savePhoto() async {
+    final result = await ImageGallerySaverPlus.saveImage(
+      imageProvider.currentImage!,
+      quality: 100,
+      name: "${DateTime.now().millisecondsSinceEpoch}",
+    );
+    if (!mounted) return false;
+    if (result['isSuccess']) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Image saved to Gallery'),
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Something went wrong!'),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,26 +62,87 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         actions: [
           TextButton(
-            onPressed: () {},
+            onPressed: () {
+              _savePhoto();
+            },
             child: Text('Save'),
           ),
         ],
       ),
-      body: Center(
-        child: Consumer<AppImageProvider>(
-          builder: (BuildContext context, value, Widget? child) {
-            if (value.currentImage != null) {
-              return Image.memory(
-                value.currentImage!,
-                // fit: BoxFit.fill,
+      body: Stack(
+        children: [
+          Center(
+            child: Consumer<AppImageProvider>(
+              builder: (BuildContext context, value, Widget? child) {
+                if (value.currentImage != null) {
+                  return Image.memory(
+                    value.currentImage!,
+                    // fit: BoxFit.fill,
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Consumer<AppImageProvider>(
+                builder: (BuildContext context, value, Widget? child) {
+              return Container(
+                margin: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.black,
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        imageProvider.undo();
+                      },
+                      icon: Icon(
+                        Icons.undo,
+                        color: imageProvider.canUndo
+                            ? Colors.white
+                            : Colors.white10,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        imageProvider.redo();
+                      },
+                      icon: Icon(
+                        Icons.redo,
+                        color: imageProvider.canRedo
+                            ? Colors.white
+                            : Colors.white10,
+                      ),
+                    ),
+                  ],
+                ),
               );
-            }
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ),
+            }),
+          )
+        ],
       ),
+      // body: Center(
+      //   child: Consumer<AppImageProvider>(
+      //     builder: (BuildContext context, value, Widget? child) {
+      //       if (value.currentImage != null) {
+      //         return Image.memory(
+      //           value.currentImage!,
+      //           // fit: BoxFit.fill,
+      //         );
+      //       }
+      //       return Center(
+      //         child: CircularProgressIndicator(),
+      //       );
+      //     },
+      //   ),
+      // ),
       bottomNavigationBar: Container(
         width: double.infinity,
         height: 60,
